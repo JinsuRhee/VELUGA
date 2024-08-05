62f3f2416f658bcce7df6c32c593baf3bdbab8f2
JinsuRhee <jinsu.rhee@gmail.com>
2024-08-05 19:29:50 +0900

    rtd testing5

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from numpy.distutils.core import Extension, setup
+
+
+# Fortran extension
+fortran_extension = Extension(
+	name=[	'src.fortran.get_ptcl_py', 
+			'src.get_cell_py',
+			'src.fortran.get_amr_py',
+			'src.fortran.find_domain_py'], 
+	sources=[	'src/fortran/get_ptcl_py.f90',
+				'src/fortran/get_cell_py.f90',
+				'src/fortran/get_amr_py.f90',
+				'src/fortran/find_domain_py.f90'], )
+
+setup(
+	name='VELUGA', 
+	version='0.01',
+	packages=find_packages(),
+	ext_modules=[fortran_extension],
+	install_requires=['numpy', 'scipy', 'pickle5', 'h5py'],
+	)
