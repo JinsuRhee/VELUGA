fa8dba474032b5a166f486d6b22a1353e31ae1e2
JinsuRhee <jinsu.rhee@gmail.com>
2024-08-07 13:45:01 +0900

    (veluga-python) r_gal and r_part ftn done2

--- veluga.py
+++ veluga.py
@@ -943,32 +943,31 @@ class veluga:
 			Retrieve physical time unit for the age of particles
 			Stored in gyr, sfact, and redsh tag
 
-		Outputs
-		-------
-		xx, yy, zz : position [kpc (physical)]
-		vx, vy, vz : velocity [km/s]
-		mp : mass [Msun]
-		ap : birth time [simulation unit]
-		zp : metallicitiy
-		gyr : birth time in Gyr
-			retrieved if g_ptime=True
-		sfact: birth time in scale factor
-			retrieved if g_ptime=True
-		redsh: birth time in redshfit
-			retrieved if g_ptime=True
-		id : Particle ID
-		domain : mpi domain number to which particles belong
-		KE : specific Kinetic energy [km^2/s^2]
-			retrieved when g_potential is called (Not implemented yet)
-		UE : specific Internal energy [km^2/s^2]
-			retrieved when g_potential is called (Not implemented yet)
-		PE : specific Potential energy [km^2/s^2]
-			retrieved when g_potential is called (Not implemented yet)
-
-
 		Returns
 		-------
 		numpy array
+			xx, yy, zz : position [kpc (physical)]
+			vx, vy, vz : velocity [km/s]
+			mp : mass [Msun]
+			ap : birth time [simulation unit]
+			zp : metallicitiy
+			gyr : birth time in Gyr
+				retrieved if g_ptime=True
+			sfact: birth time in scale factor
+				retrieved if g_ptime=True
+			redsh: birth time in redshfit
+				retrieved if g_ptime=True
+			id : Particle ID
+			domain : mpi domain number to which particles belong
+			KE : specific Kinetic energy [km^2/s^2]
+				retrieved when g_potential is called (Not implemented yet)
+			UE : specific Internal energy [km^2/s^2]
+				retrieved when g_potential is called (Not implemented yet)
+			PE : specific Potential energy [km^2/s^2]
+				retrieved when g_potential is called (Not implemented yet)
+
+
+
 	
 		Examples
 		--------
